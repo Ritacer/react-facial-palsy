d5382c414658c24fb150b7a9e8d6a8801b038eb9
Ritacer <59940899+Ritacer@users.noreply.github.com>
2020-04-10 20:13:46 +0100

    Updated MimicVideo.js with my fb video URL

--- src/MimicVideo.js
+++ src/MimicVideo.js
@@ -5,7 +5,7 @@ import { Player } from "video-react";
 export default function MimicVideo() {
   return (
     <Player>
-      <source src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4" />
+      <source src="https://www.facebook.com/rita.cerqueira.39/videos/vb.754746693/10158488482301694/?type=2&theater&notif_t=video_processed&notif_id=1586545476100798" />
     </Player>
   );
 }
