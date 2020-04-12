bff000cad38df1fd0fbc09aba08c6686e57264c1
Ritacer <59940899+Ritacer@users.noreply.github.com>
2020-04-12 17:01:40 +0100

    Reloaded the videos

--- src/ForeheadVideo.js
+++ src/ForeheadVideo.js
@@ -5,7 +5,7 @@ import { Player } from "video-react";
 export default function ForeheadVideo() {
   return (
     <Player>
-      <source src="https://video.fopo1-1.fna.fbcdn.net/v/t42.9040-2/93515553_2905964176146592_5721245979890417664_n.mp4?_nc_cat=111&_nc_sid=985c63&efg=eyJybHIiOjMxMywicmxhIjo1MTIsInZlbmNvZGVfdGFnIjoic3ZlX3NkIn0%3D&_nc_ohc=lNG2j42HLOgAX-PNdGi&rl=313&vabr=174&_nc_ht=video.fopo1-1.fna&oh=1db7cfa62d81d809c14164e27401c129&oe=5E92379C" />
+      <source src="https://video-mad1-1.xx.fbcdn.net/v/t42.9040-2/93515553_2905964176146592_5721245979890417664_n.mp4?_nc_cat=111&_nc_sid=985c63&efg=eyJybHIiOjMxMywicmxhIjo1MTIsInZlbmNvZGVfdGFnIjoic3ZlX3NkIn0%3D&_nc_ohc=lNG2j42HLOgAX-PNdGi&rl=313&vabr=174&_nc_ht=video-mad1-1.xx&oh=b0c1423d06221b4168efaeee0e7e552b&oe=5E935EEC" />
     </Player>
   );
 }
