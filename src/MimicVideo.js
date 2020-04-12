78f831c3a2af4a74ba010bb9ab8e4d399907ea0e
Ritacer <59940899+Ritacer@users.noreply.github.com>
2020-04-12 15:30:00 +0100

    Updated video addresses

--- src/MimicVideo.js
+++ src/MimicVideo.js
@@ -5,7 +5,7 @@ import { Player } from "video-react";
 export default function MimicVideo() {
   return (
     <Player>
-      <source src="https://video.fopo1-1.fna.fbcdn.net/v/t42.9040-2/92832974_257056932361507_1546021259706368000_n.mp4?_nc_cat=108&_nc_sid=985c63&efg=eyJybHIiOjMwMCwicmxhIjo1MTIsInZlbmNvZGVfdGFnIjoic3ZlX3NkIn0%3D&_nc_ohc=LqMeMR6okucAX-tn79f&rl=300&vabr=147&_nc_ht=video.fopo1-1.fna&oh=a779350b6b7a5d0cd5c76ab97c28adcf&oe=5E92347E" />
+      <source src="https://video-mad1-1.xx.fbcdn.net/v/t42.9040-2/92832974_257056932361507_1546021259706368000_n.mp4?_nc_cat=108&_nc_sid=985c63&efg=eyJybHIiOjMwMCwicmxhIjo1MTIsInZlbmNvZGVfdGFnIjoic3ZlX3NkIn0%3D&_nc_ohc=LqMeMR6okucAX-tn79f&rl=300&vabr=147&_nc_ht=video-mad1-1.xx&oh=a961fdc2e6bc061e324fb2b646df095d&oe=5E934DBE" />
     </Player>
   );
 }
