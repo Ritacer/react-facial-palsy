4ee61990734229c6633c70ffad6dff5b8aaf6fd0
Ritacer <59940899+Ritacer@users.noreply.github.com>
2020-04-12 15:23:11 +0100

    Styled the project

--- src/App.js
+++ src/App.js
@@ -9,42 +9,47 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Paralisia Facial</h1>
-        <div className="row">
-          <div className="col-4">
-            <h2>Mímica facial</h2>
-            <MimicVideo />
+        <div className="newContainer">
+          <h1>Paralisia Facial</h1>
+          <div className="row">
+            <div className="col-4">
+              <h2>Mímica facial</h2>
+              <MimicVideo />
+            </div>
+          </div>
+          <h2>Estimulação com gelo</h2>
+          <div className="row">
+            <div className="col-4">
+              <p>Estimulação da testa</p>
+              <ForeheadVideo />
+            </div>
+            <div className="col-4">
+              <p>Estimulação do olho</p>
+              <EyeVideo />
+            </div>
+            <div className="col-4">
+              <p>Estimulação da boca e nariz</p>
+              <MouthNoseVideo />
+            </div>
           </div>
         </div>
-        <h2>Estimulação com gelo</h2>
-        <div className="row">
-          <div className="col-4">
-            <p>Estimulação da testa</p>
-            <ForeheadVideo />
-          </div>
-          <div className="col-4">
-            <p>Estimulação do olho</p>
-            <EyeVideo />
-          </div>
-          <div className="col-4">
-            <p>Estimulação da boca e nariz</p>
-            <MouthNoseVideo />
-          </div>
-        </div>
+        <footer>
+          <a
+            href="https://github.com/Ritacer/react-facial-palsy"
+            target="_blank"
+          >
+            Open-source
+          </a>{" "}
+          code by{" "}
+          <a
+            href="https://www.linkedin.com/in/rita-cerqueira-20198766"
+            target="_blank"
+          >
+            Rita Cerqueira
+          </a>
+          , 2020.
+        </footer>
       </div>
-      <footer>
-        <a href="https://github.com/Ritacer/react-facial-palsy" target="_blank">
-          Open-source
-        </a>{" "}
-        code by{" "}
-        <a
-          href="https://www.linkedin.com/in/rita-cerqueira-20198766"
-          target="_blank"
-        >
-          Rita Cerqueira
-        </a>
-        , 2020.
-      </footer>
     </div>
   );
 }
