50f52e9809ca7d7a6eb1f18acc2bd80e372cf1da
Ritacer <59940899+Ritacer@users.noreply.github.com>
2020-04-12 16:19:48 +0100

    Changed video element

--- src/EyeVideo.js
+++ src/EyeVideo.js
@@ -4,8 +4,9 @@ import { Player } from "video-react";
 
 export default function EyeVideo() {
   return (
-    <Player>
-      <source src="https://video.fopo1-1.fna.fbcdn.net/v/t42.9040-2/92991474_693535984735930_6435654742369632256_n.mp4?_nc_cat=102&_nc_sid=985c63&efg=eyJybHIiOjM0MCwicmxhIjo1MTIsInZlbmNvZGVfdGFnIjoic3ZlX3NkIn0%3D&_nc_ohc=ri72aZg6B0kAX_PHRXP&rl=340&vabr=189&_nc_ht=video.fopo1-1.fna&oh=d7fb011975eb91373323da78783a6618&oe=5E922F41" />
-    </Player>
+    <Player
+      playsInline
+      src="https://video.fopo1-1.fna.fbcdn.net/v/t42.9040-2/92991474_693535984735930_6435654742369632256_n.mp4?_nc_cat=102&_nc_sid=985c63&efg=eyJybHIiOjM0MCwicmxhIjo1MTIsInZlbmNvZGVfdGFnIjoic3ZlX3NkIn0%3D&_nc_ohc=ri72aZg6B0kAX_PHRXP&rl=340&vabr=189&_nc_ht=video.fopo1-1.fna&oh=d7fb011975eb91373323da78783a6618&oe=5E922F41"
+    />
   );
 }
--- src/MimicVideo.js
+++ src/MimicVideo.js
@@ -4,8 +4,6 @@ import { Player } from "video-react";
 
 export default function MimicVideo() {
   return (
-    <Player>
-      <source src="https://video-mad1-1.xx.fbcdn.net/v/t42.9040-2/92832974_257056932361507_1546021259706368000_n.mp4?_nc_cat=108&_nc_sid=985c63&efg=eyJybHIiOjMwMCwicmxhIjo1MTIsInZlbmNvZGVfdGFnIjoic3ZlX3NkIn0%3D&_nc_ohc=LqMeMR6okucAX-tn79f&rl=300&vabr=147&_nc_ht=video-mad1-1.xx&oh=a961fdc2e6bc061e324fb2b646df095d&oe=5E934DBE" />
-    </Player>
+    <Player src="https://video-mad1-1.xx.fbcdn.net/v/t42.9040-2/92832974_257056932361507_1546021259706368000_n.mp4?_nc_cat=108&_nc_sid=985c63&efg=eyJybHIiOjMwMCwicmxhIjo1MTIsInZlbmNvZGVfdGFnIjoic3ZlX3NkIn0%3D&_nc_ohc=LqMeMR6okucAX-tn79f&rl=300&vabr=147&_nc_ht=video-mad1-1.xx&oh=a961fdc2e6bc061e324fb2b646df095d&oe=5E934DBE" />
   );
 }
