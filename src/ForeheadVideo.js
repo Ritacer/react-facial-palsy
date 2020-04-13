64ab6fa8565f6dd4da281e3b67f5df489fd69b20
Ritacer <59940899+Ritacer@users.noreply.github.com>
2020-04-13 14:49:01 +0100

    Added video control bar property

--- src/ForeheadVideo.js
+++ src/ForeheadVideo.js
@@ -1,11 +1,12 @@
 import React from "react";
 
-import { Player } from "video-react";
+import { Player, ControlBar } from "video-react";
 
 export default function ForeheadVideo() {
   return (
     <Player>
       <source src="https://video-mad1-1.xx.fbcdn.net/v/t42.9040-2/93515553_2905964176146592_5721245979890417664_n.mp4?_nc_cat=111&_nc_sid=985c63&efg=eyJybHIiOjMxMywicmxhIjo1MTIsInZlbmNvZGVfdGFnIjoic3ZlX3NkIn0%3D&_nc_ohc=lNG2j42HLOgAX-PNdGi&rl=313&vabr=174&_nc_ht=video-mad1-1.xx&oh=b0c1423d06221b4168efaeee0e7e552b&oe=5E935EEC" />
+      <ControlBar autoHide={false} />
     </Player>
   );
 }
