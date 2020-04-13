afacc895a9373127e53fbfaa7944f3b3651ab00f
Ritacer <59940899+Ritacer@users.noreply.github.com>
2020-04-13 15:50:38 +0100

    Added the correct video urls

--- src/responsive-player.js
+++ src/responsive-player.js
@@ -3,12 +3,12 @@ import React from "react";
 import ReactPlayer from "react-player";
 import "./responsive-player.css";
 
-const ResponsivePlayer = () => {
+const ResponsivePlayer = ({ url }) => {
   return (
     <div className="player-wrapper">
       <ReactPlayer
         className="react-player"
-        url="https://youtu.be/_27eD49ePQE"
+        url={url}
         width="100%"
         height="100%"
         controls={true}
