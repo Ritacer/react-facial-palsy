1610ac72aa4142cb352fac0cacefeaccf31eea4b
Ritacer <59940899+Ritacer@users.noreply.github.com>
2020-04-18 18:36:17 +0100

    Changed all the videos

--- src/FacialPalsy.js
+++ src/FacialPalsy.js
@@ -12,28 +12,28 @@ export default function FacialPalsy() {
         </div>
         <div className="main">
           <h2>Mímica facial</h2>
-          <ResponsivePlayer url="92832974_257056932361507_1546021259706368000_n.mp4" />
+          <ResponsivePlayer url="mipt93886162_267201794457945_94678817418772480_n.mp4" />
         </div>
         <h2>Estimulação com gelo</h2>
         <div className="row">
           <div className="col-6">
             <p>Estimulação da testa</p>
-            <ResponsivePlayer url="93515553_2905964176146592_5721245979890417664_n.mp4" />
+            <ResponsivePlayer url="fopt94185452_255673565567968_8459799694271315968_n.mp4" />
           </div>
           <div className="col-6">
             <p>Estimulação do olho</p>
-            <ResponsivePlayer url="92991474_693535984735930_6435654742369632256_n.mp4" />
+            <ResponsivePlayer url="eypt94163985_2636973926534545_1318350008327602176_n.mp4" />
           </div>
         </div>
         <div className="row">
           <div className="col-6">
             <p>Estimulação da boca e nariz</p>
-            <ResponsivePlayer url="92996122_218388046156541_8443100608021397504_n.mp4" />
+            <ResponsivePlayer url="mnpt94055569_681992159223897_1985186390186393600_n.mp4" />
           </div>
         </div>
         <div className="massage">
           <h2>Massagem da face</h2>
-          <ResponsivePlayer url="92996122_218388046156541_8443100608021397504_n.mp4" />
+          <ResponsivePlayer url="mapt92871985_843368219407879_6363956469590654976_n.mp4" />
         </div>
       </div>
     </div>
